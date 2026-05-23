b56dd0846bf33e5e6d492ad19a9e484b57bd8e89
Jun Bosque <bosque.junpaul@gmail.com>
2026-05-23 11:14:36 +0800

    feat: add DNS sync executor

---
+++ packages/registry/src/core/sync.ts
@@ -0,0 +1,47 @@
+import { type DNSAction } from "@is-pinoy/schemas";
+import {
+  createRecord,
+  updateRecord,
+  deleteRecord,
+} from "../providers/cloudflare/client.js";
+
+function logAction(action: DNSAction) {
+  switch (action.type) {
+    case "CREATE":
+      console.log(
+        `[DRY RUN] CREATE ${action.fqdn} \u2192 ${action.record.type} \u2192 ${action.record.value}`,
+      );
+      break;
+    case "UPDATE":
+      console.log(
+        `[DRY RUN] UPDATE ${action.fqdn} (${action.id}) \u2192 ${action.record.type} \u2192 ${action.record.value}`,
+      );
+      break;
+    case "DELETE":
+      console.log(`[DRY RUN] DELETE ${action.fqdn} (${action.id})`);
+      break;
+  }
+}
+
+export async function sync(actions: DNSAction[], isDryRun = false) {
+  for (const action of actions) {
+    if (isDryRun) {
+      logAction(action);
+      continue;
+    }
+    switch (action.type) {
+      case "CREATE":
+        await createRecord(action.record, action.fqdn);
+        console.log(`CREATED ${action.fqdn}`);
+        break;
+      case "UPDATE":
+        await updateRecord(action.id, action.record, action.fqdn);
+        console.log(`UPDATED ${action.fqdn}`);
+        break;
+      case "DELETE":
+        await deleteRecord(action.id);
+        console.log(`DELETED ${action.fqdn}`);
+        break;
+    }
+  }
+}
