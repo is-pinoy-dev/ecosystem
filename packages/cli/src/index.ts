34f4f67037eaa4a9452154b85cdec48c1f8f1663
Jun Bosque <bosque.junpaul@gmail.com>
2026-05-23 12:52:28 +0800

    feat(cli): wire up CLI entry point with commander

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,13 @@
-export {}
+#!/usr/bin/env node
+
+import { program } from "commander";
+import { registerRegistryCommand } from "./commands/registry/index.js";
+
+program
+  .name("is-pinoy")
+  .description("CLI for managing the Philippine domain DNS registry")
+  .version("0.0.0");
+
+registerRegistryCommand(program);
+
+program.parse();
