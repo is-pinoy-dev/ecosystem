239e2207a1ed85a491f5f90402eed5c53003fd1e
Jun Bosque <bosque.junpaul@gmail.com>
2026-05-23 13:23:00 +0800

    feat(cli): add global --dotenv option to load env file

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+import fs from "fs";
 import { program } from "commander";
+import { config as loadEnv } from "dotenv";
 import { SchemaError } from "@is-pinoy/registry";
 import { registerRegistryCommand } from "./commands/registry/index.js";
 import { printSchemaError, printBanner, error } from "./utils/output.js";
@@ -10,11 +12,22 @@ const VERSION = "0.0.0";
 program
   .name("is-pinoy")
   .description("CLI for managing the Philippine domain DNS registry")
-  .version(VERSION);
+  .version(VERSION)
+  .option("--dotenv <path>", "Path to a .env file to load");
 
 registerRegistryCommand(program);
 
-program.hook("preAction", () => printBanner(VERSION));
+program.hook("preAction", (thisCommand) => {
+  const envFile = thisCommand.optsWithGlobals().dotenv as string | undefined;
+  if (envFile) {
+    if (!fs.existsSync(envFile)) {
+      error(`env file not found: ${envFile}`);
+      process.exit(1);
+    }
+    loadEnv({ path: envFile, override: true, quiet: true });
+  }
+  printBanner(VERSION);
+});
 
 program.parseAsync().catch((err) => {
   if (err instanceof SchemaError) {
